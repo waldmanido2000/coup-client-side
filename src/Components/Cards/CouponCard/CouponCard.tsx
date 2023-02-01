19ac19a14922dcba84b6263ac48c3828ffd9982a
waldmanido2000 <112062207+waldmanido2000@users.noreply.github.com>
2023-02-01 20:50:30 +0200

    fixed redux

--- src/Components/Cards/CouponCard/CouponCard.tsx
+++ src/Components/Cards/CouponCard/CouponCard.tsx
@@ -7,12 +7,14 @@ interface CouponProps {
 
 function CouponCard(props:CouponProps): JSX.Element {
     return (
-        <div className="CouponCard row">
+        <div className="CouponCard box">
 			<h4>{props.coupon.title}</h4>
-			<h4>{props.coupon.title}</h4>
-			<h4>{props.coupon.title}</h4>
-			<h4>{props.coupon.title}</h4>
-			<h4>{props.coupon.title}</h4>
+			<p>{props.coupon.description}</p>
+			<p>{props.coupon.category}</p>
+			<p>{props.coupon.startDate}</p>
+			<p>{props.coupon.endDate}</p>
+			<p>{props.coupon.price}</p>
+			<p>{props.coupon.amount}</p>
         </div>
     );
 }
--- src/Components/ClientArea/AdminArea/CompanyList/CompanyList.tsx
+++ src/Components/ClientArea/AdminArea/CompanyList/CompanyList.tsx
@@ -11,35 +11,29 @@ import "./CompanyList.css";
 
 function CompanyList(): JSX.Element {
     const navigate = useNavigate();
-    const [companies, setCompanies] = useState<CompanyModel[]>(store.getState().companiesReducer.companies);
+    const [companies, setCompanies] = useState<CompanyModel[]>([]);
     useEffect(() => {
         // const token = store.getState().userReducer.user.token;
-        // console.log(token);
         // if (!token) {
         //     navigate("/login");
         // }
-        
-        if (companies.length === 0) {
-                webApi.getAllCompanies()
-                    .then(res => {
-                        console.log(res.data);
-                        // Update local state
-                        setCompanies(res.data);
 
-                        // Update app state
+        webApi.getAllCompanies()
+            .then(res => {
+                // Update local state
+                setCompanies(res.data);
 
-                        store.dispatch(gotAllCompaniesAction(res.data));
+                // Update app state
+                store.dispatch(gotAllCompaniesAction(res.data));
 
-                        notify.success('Woho I got my element from server side!!!')
-                    })
-                    .catch(err => notify.error(err));
+                // notify.success('Woho I got my element from server side!!!');
+            })
+            .catch(err => notify.error(err));
 
-        }
         return store.subscribe(() => {
             setCompanies(store.getState().companiesReducer.companies);
         });
     }, []);
-
 
     return (
         <div className="CompanyList col">
