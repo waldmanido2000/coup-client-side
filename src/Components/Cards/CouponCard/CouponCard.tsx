1d478693aa452ede286461e8101e8ba0b9e2ee1f
waldmanido2000 <112062207+waldmanido2000@users.noreply.github.com>
2023-02-06 17:08:54 +0200

    background-fixes

--- src/Components/Cards/CouponCard/CouponCard.tsx
+++ src/Components/Cards/CouponCard/CouponCard.tsx
@@ -1,36 +1,62 @@
 import { useState } from "react";
 import { CouponModel } from "../../../Models/CouponModel";
+import notFoundImage from "../../..//Assets/not-found.jpg";
 import "./CouponCard.css";
 
 interface CouponProps {
-  coupon: CouponModel;
+	coupon: CouponModel;
 }
 
 function CouponCard(props: CouponProps): JSX.Element {
-  const [imageUrl, setImageUrl] = useState(props.coupon.image);
+	const [imageUrl, setImageUrl] = useState(props.coupon.image);
+	const [imageError, setImageError] = useState(false);
 
-  const handleImageError = () => {
-    setImageUrl("../../../src/Assets/not-found.jpg");
-  };
+	const handleImageError = () => {
+		setImageError(true);
+	};
+	const categoryClass = (category: string) => {
+		switch ( category.toLowerCase()) {
+			case "cars":
+				return "car";
+			case "restaurant":
+				return "restaurant";
+			case "food":
+				return "food";
+			case "electricity":
+				return "electricity";
+			case "vacation":
+				return "vacation";
+			case "clothes":
+				return "clothes";
+			default:
+				return "";
+		}
+	};
 
-  return (
-    <div className="CouponCard">
-      <h4>{props.coupon.title}</h4>
-      <div className="row">
-        <div>
-          <p>description: {props.coupon.description}</p>
-          <p>category: {props.coupon.category}</p>
-          <p>available since: {props.coupon.startDate}</p>
-          <p>ends in: {props.coupon.endDate}</p>
-          <p>cost: {props.coupon.price}</p>
-          <p>left to purchase: {props.coupon.amount}</p>
-        </div>
-        <div>
-          <img src={imageUrl} alt={props.coupon.image} onError={handleImageError} />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+
+		<div className={`CouponCard ${categoryClass(props.coupon.category)}`}>
+			<h4>{props.coupon.title}</h4>
+			<div className="row">
+				<div>
+					<p>description: {props.coupon.description}</p>
+					<p>category: {props.coupon.category}</p>
+					<p>available since: {props.coupon.startDate}</p>
+					<p>ends in: {props.coupon.endDate}</p>
+					<p>cost: {props.coupon.price}</p>
+					<p>left to purchase: {props.coupon.amount}</p>
+				</div>
+				<div>
+					{
+						imageError ?
+							<img src={notFoundImage} alt={props.coupon.image} />
+							:
+							<img src={imageUrl} alt={props.coupon.image} onError={handleImageError} />
+					}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default CouponCard;
--- src/Components/Cards/PurchaseCard/PurchaseCard.tsx
+++ src/Components/Cards/PurchaseCard/PurchaseCard.tsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
 import { CouponModel } from "../../../Models/CouponModel";
+import notFoundImage from "../../..//Assets/not-found.jpg";
 import "./PurchaseCard.css";
 interface PurchaseProps {
 	purchase: CouponModel;
 }
 
 function PurchaseCard(props:PurchaseProps): JSX.Element {
+	const [imageUrl, setImageUrl] = useState(props.purchase.image);
+	const [imageError, setImageError] = useState(false);
+
+	const handleImageError = () => {
+		setImageError(true);
+	};
+	const categoryClass = (category: string) => {
+		switch ( category.toLowerCase()) {
+			case "cars":
+				return "car";
+			case "restaurant":
+				return "restaurant";
+			case "food":
+				return "food";
+			case "electricity":
+				return "electricity";
+			case "vacation":
+				return "vacation";
+			case "clothes":
+				return "clothes";
+			default:
+				return "";
+		}
+	};
     return (
-        <div className="PurchaseCard">
+		<div className={`PurchaseCard ${categoryClass(props.purchase.category)}`}>
 						<h4>{props.purchase.title}</h4>
 			<div className="row">
 				<div>
@@ -17,7 +43,12 @@ function PurchaseCard(props:PurchaseProps): JSX.Element {
 					<p>cost: {props.purchase.price}</p>
 				</div>
 				<div>
-					<img src={props.purchase.image} alt={props.purchase.image} />
+					{
+						imageError ?
+							<img src={notFoundImage} alt={props.purchase.image} />
+							:
+							<img src={imageUrl} alt={props.purchase.image} onError={handleImageError} />
+					}
 				</div>
 			</div>
         </div>
